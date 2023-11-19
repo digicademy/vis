0614585bca7f0e4fef3c89858ac2e35f2c200502
Jonatan Jalle Steller <jonatan.steller@adwmainz.de>
2023-11-19 23:12:26 +0100

    Revise requirements

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,8 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version'        => '0.1.0',
     'constraints'    => [
         'depends'   => [
-            'typo3' => '12.0.0-12.4.99',
-            'php'   => '8.1.0-8.2.99'
+            'typo3' => '12.0.0-12.99.99'
         ],
         'conflicts' => [
         ],
